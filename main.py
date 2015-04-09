8c707ec4337a35a6331d2f3d491ca36b8ed9e755
Nate BeDell <nabedell@liberty.edu>
2015-04-09 14:20:27 -0400

    Added threading

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 
 import sys
 import subprocess
+import threading
+
 try:
  	import pygtk
   	pygtk.require("2.0")
@@ -20,27 +22,34 @@ class Application():
     def __init__(self):
         #Set the Glade file
         self.gladefile = "UI.glade"  
-        
-
         self.builder = gtk.Builder()
         self.builder.add_from_file(self.gladefile)
         self.builder.connect_signals(self)
         self.main_window = self.builder.get_object("Main Window") 
-
+        self.running = False
 
         self.main_window.show_all()
 
     def connectHandler(self,x):
-        pass
+        if(self.running):
+            thread = threading.Thread(target=lambda: subprocess.call(['x-www-browser','localhost']))
+            thread.start()
 
     def startHandler(self,x):
-        subprocess.call(['sudo','apachectl','start'])
+        if not self.running:
+            thread = threading.Thread(target= lambda: subprocess.call(['gksudo','apachectl','start']))
+            thread.start()
+            self.running = True
 
     def restartHandler(self,x):
-        subprocess.call(['sudo','apachectl','restart'])
+        thread = threading.Thread(target=lambda: subprocess.call(['gksudo','apachectl','restart']))
+        thread.start()
+        self.running = True
 
     def stopHandler(self,x):
-        subprocess.call(['sudo','apachectl','stop'])
+        thread = threading.Thread(target=lambda: subprocess.call(['gksudo','apachectl','stop']))
+        thread.start()
+        self.running = False
 
 def main():
     # Initalize and run the application
