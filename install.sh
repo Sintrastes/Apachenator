1126b500cf1fa11bfbaa426fb234d71894f155e2
Nate BeDell <nabedell@liberty.edu>
2015-04-20 23:48:31 -0400

    Changed stuff

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+sudo apt-get install apache2 python
+
 sudo mkdir /opt/apachenator
 sudo cp ./Apachenator.desktop ~/.local/share/applications/Apachenator.desktop
 sudo cp -R ./ /opt/apachenator/
--- main.py
+++ main.py
@@ -32,8 +32,11 @@ class Application():
         self.builder.connect_signals(self)
         self.main_window = self.builder.get_object("Main Window") 
         self.fixed1 = self.builder.get_object("fixed1") 
+        self.status = self.builder.get_object("Status")
         self.running = False
         self.fixed1.connect('expose-event', draw_pixbuf)
+
+        self.status.set_text("Not Connected")
 
         self.button = self.builder.get_object("button1")
         label = self.button.child
@@ -63,7 +66,12 @@ class Application():
         self.status = self.builder.get_object("Status")
         self.status.modify_font(pango.FontDescription("eufm10 Bold 26"))
         self.status.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse('#000000'))
-
+        try:
+            if subprocess.check_output(["service","apache2","status"]) == ' * apache2 is running\n':
+                self.running = True
+                self.status.set_text("Connected")
+        except:
+            pass
         self.main_window.show_all()
 
     def connectHandler(self,x):
@@ -72,20 +80,36 @@ class Application():
             thread.start()
 
     def startHandler(self,x):
+        def threadFunction(ref):
+            ref.status.set_text("Connecting...")
+            subprocess.call(['gksudo','apachectl','start'])
+            ref.running = True
+            ref.status.set_text("Connected")
         if not self.running:
-            thread = threading.Thread(target= lambda: subprocess.call(['gksudo','apachectl','start']))
+            thread = threading.Thread(target=threadFunction(self))
             thread.start()
-            self.running = True
 
     def restartHandler(self,x):
-        thread = threading.Thread(target=lambda: subprocess.call(['gksudo','apachectl','restart']))
+        def threadFunction(ref):
+            ref.status.set_text("Reconnecting...")
+            subprocess.call(['gksudo','apachectl','restart'])
+            ref.running = True
+            ref.status.set_text("Connected")
+        thread = threading.Thread(target=threadFunction(self))
         thread.start()
-        self.running = True
+        
 
     def stopHandler(self,x):
-        thread = threading.Thread(target=lambda: subprocess.call(['gksudo','apachectl','stop']))
+        def threadFunction(ref):
+            ref.status.set_text("Disconnecting...")
+            subprocess.call(['gksudo','apachectl','stop'])
+            ref.running = False
+            ref.status.set_text("Not Connecting")
+            self.running = False
+            self.status.set_text("Not Connected")
+        thread = threading.Thread(target=threadFunction(self))
         thread.start()
-        self.running = False
+
 
 def main():
     # Initalize and run the application
